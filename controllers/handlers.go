a5fb21cc6c138ee433c8f68ef0937d2f1ff5a2b9
vinomondi <vincentomondi1002@gmail.com>
2024-08-07 16:40:24 +0300

    feat: added API handlers with controllers/handlers.go file

---
+++ controllers/handlers.go
@@ -0,0 +1,61 @@
+// handler.go
+package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/Vincent-Omondi/groupie-tracker/api"
+)
+
+// GetArtistsHandler handles the /artists route
+func GetArtistsHandler(w http.ResponseWriter, r *http.Request) {
+	artists, err := api.GetArtists()
+	if err != nil {
+		http.Error(w, "Failed to fetch artists", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(artists); err != nil {
+		http.Error(w, "Failed to encode locations", http.StatusInternalServerError)
+	}
+}
+
+// GetLocationsHandler handles the /locations route
+func GetLocationsHandler(w http.ResponseWriter, r *http.Request) {
+	locations, err := api.GetLocations()
+	if err != nil {
+		http.Error(w, "Failed to fetch locations", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(locations); err != nil {
+		http.Error(w, "Failed to encode locations", http.StatusInternalServerError)
+	}
+}
+
+// GetDatesHandler handles the /dates route
+func GetDatesHandler(w http.ResponseWriter, r *http.Request) {
+	dates, err := api.GetDates()
+	if err != nil {
+		http.Error(w, "Failed to fetch dates", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(dates); err != nil {
+		http.Error(w, "Failed to encode locations", http.StatusInternalServerError)
+	}
+}
+
+// GetRelationsHandler handles the /relations route
+func GetRelationsHandler(w http.ResponseWriter, r *http.Request) {
+	relations, err := api.GetRelations()
+	if err != nil {
+		http.Error(w, "Failed to fetch relations", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(relations); err != nil {
+		http.Error(w, "Failed to encode locations", http.StatusInternalServerError)
+	}
+}
